915107a2d34f066dad7b3cfbec9a6c233727619c
Tristan Ferré <tristanferre@yahoo.fr>
2016-01-26 15:34:13 +0100

    setting endpoint of cellphone

--- app/controllers/api/actions_controller.rb
+++ app/controllers/api/actions_controller.rb
@@ -1,7 +1,8 @@
 class Api::ActionsController < ApplicationController
 
-  def new
-
+  def set_wallpaper
+    wallpaper = params[:wallpaper]
+    RestClient.post "endoint_de_guillaume", wallpaper: wallpaper #TODO entrer le godamn endpoint de guillaume (à entrer dans la config par la suite)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :authorizations
   
   resources :actions
+
+  post 'actions/set_wallpaper', to: 'actions#set_wallpaper'
   
 end
